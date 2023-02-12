9eefc66d2439b433ffcc247dfa79a64817d80a02
jdluis <jdluisdev@gmail.com>
2023-02-12 15:01:11 +0000

    iteration 4

--- app.js
+++ app.js
@@ -1,45 +1,82 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const hbs = require('hbs');
+const express = require("express");
+const hbs = require("hbs");
 
-const SpotifyWebApi = require("spotify-web-api-node")
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
+
+// Register the location for handlebars partials here:
+const path = require("node:path");
+hbs.registerPartials(path.join(__dirname, "views/partials"));
 
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
-  });
-  
-  // Retrieve an access token
-  spotifyApi
-    .clientCredentialsGrant()
-    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
-    .catch(error => console.log('Something went wrong when retrieving an access token', error));
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body["access_token"]))
+  .catch((error) =>
+    console.log("Something went wrong when retrieving an access token", error)
+  );
 
 // Our routes go here:
 app.get("/", (req, res, next) => {
-    res.render("index")
-})
+  res.render("index");
+});
 
 app.get("/artist-search", (req, res, next) => {
-    spotifyApi
-  .searchArtists(req.query.artist)
-  .then(data => {
-    console.log(data.body.artists.items[0].images[0].url)
-    res.render("artist-search-results.hbs", {
-        artists: data.body.artists.items
+  spotifyApi
+    .searchArtists(req.query.artist)
+    .then((data) => {
+      res.render("artist-search-results.hbs", {
+        artists: data.body.artists.items,
+      });
     })
-  })
-  .catch(err => console.log('The error while searching artists occurred: ', err));
-  
-})
+    .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
+});
 
+app.get("/albums/:artistId", (req, res, next) => {
+  spotifyApi
+    .getArtistAlbums(req.params.artistId)
+    .then((data) => {
+      res.render("albums.hbs", {
+        albumns: data.body.items,
+      });
+    })
+    .catch((err) =>
+      console.log("The error while searching albumns occurred: ", err)
+    );
+});
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.get("./albums/tracks/:albumId", (req, res, next) => {
+  console.log("----------------------------------");
+  console.log(req.params.albumId);
+/*   res.render("tracks.hbs"); */
+  spotifyApi
+    .getAlbumTracks(req.params.albumId)
+    .then((data) => {
+      console.log(data.body);
+      res.render("tracks.hbs", {
+        tracks: data.body.items,
+      });
+    })
+    .catch((err) =>
+      console.log("The error while searching tracks occurred: ", err)
+    );
+});
+
+app.listen(3000, () =>
+  console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
+);
